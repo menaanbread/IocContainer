d7e95ab4d25dde0d78def46491b8fbddd6363618
Thomas Woolley <thomas.woolley92@outlook.com>
2017-02-14 22:18:07 +0000

    Cleaning

--- LifeStyle.js
+++
@@ -1,9 +0,0 @@
-var IoC;
-(function (IoC) {
-    var LifeStyle;
-    (function (LifeStyle) {
-        LifeStyle[LifeStyle["Singleton"] = 0] = "Singleton";
-        LifeStyle[LifeStyle["Transient"] = 1] = "Transient";
-    })(LifeStyle = IoC.LifeStyle || (IoC.LifeStyle = {}));
-})(IoC || (IoC = {}));
-//# sourceMappingURL=LifeStyle.js.map
